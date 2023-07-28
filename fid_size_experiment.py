55d00e346a3f100c13492b54081e29411981ce8f
Freddie Sligo-Young <fms119@ic.ac.uk>
2023-07-28 11:31:07 +0100

    FID experiment for generated images,

--- fid_size_experiment.py
+++ fid_size_experiment.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import tensorflow_datasets as tfds
 import numpy as np
-
+import gc
 from zijing_main import compute_fid_nchw
 
 sizes = np.sort(
@@ -14,20 +14,21 @@ def get_fid(n, base_size=None):
     '''
     #Data not relevant as I have saved statistics
     data_samples = 0.5*np.ones((100, 3, 32, 32))
-
-    file_path = '/vol/bitbucket/fms119/score_sde_pytorch/samples/cifar10_true_trials/cirfar10_true_50000.npz'
+    file_path = '/vol/bitbucket/fms119/score_sde_pytorch/samples/all_samples_50000.npz'
     data = np.load(file_path)
-    gen_samples = data['images'][:n].transpose(0,3,1,2)
+    gen_samples = data['images'][:n]
+    gc.collect()
     gen_samples = np.interp(gen_samples, (gen_samples.min(), gen_samples.max()), (0, 1))    
     fid = compute_fid_nchw(data_samples, gen_samples, base_size=base_size)
     return fid
 
-fid_trails = np.zeros((4, len(sizes)))
+data = np.load('/vol/bitbucket/fms119/score_sde_pytorch/samples/cifar10_true_trials/50k_train_vs_n_train.npz')
+fid_trails = data['fid_trails']
 
 for j ,base_size in enumerate(['10k', '50k']):
     for i, n in enumerate(sizes):
+        J = j + 2
         fid = get_fid(n, base_size)    
-        fid_trails[j, i] = fid
-        print(fid_trails.round(2))
+        fid_trails[J, i] = fid
         # You should always be saving progess during experiments like this.
-        np.savez('/vol/bitbucket/fms119/score_sde_pytorch/samples/cifar10_true_trials/50k_train_vs_n_train.npz', fid_trails=fid_trails)
+        np.savez('/vol/bitbucket/fms119/score_sde_pytorch/samples/cifar10_true_trials/fid_size_experiment.npz', fid_trails=fid_trails)
