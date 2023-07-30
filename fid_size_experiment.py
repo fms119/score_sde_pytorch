54451d1cc09b96bebeb98ba7d146e5f5d5885ae2
Freddie Sligo-Young <fms119@ic.ac.uk>
2023-07-30 12:25:20 +0100

    FID var at 4000 experiment

--- fid_size_experiment.py
+++ fid_size_experiment.py
@@ -4,13 +4,15 @@ import numpy as np
 import gc
 from zijing_main import compute_fid_nchw
 
-sizes = np.sort(
-    [500,  2000,  4000,  6000,  8000, 10001, 15000,  1000, 12500, 20000,] 
-    + list(range(30000, 60000, 10000)))
+n_trials = 10
+n=4000
+fid_trails = np.zeros(n_trials)
+base_size = '50k'
+source = 'generated'
 
-def get_fid(n, base_size=None, source='choose'):
+def get_fid(i, n, base_size=None, source='choose'):
     '''
-    base_size: str: '10k' or '50k' or None
+    base_size: str: '2k', '10k' or '50k' or None
     '''
     #Data not relevant as I have saved statistics
     data_samples = 0.5*np.ones((100, 3, 32, 32))
@@ -19,7 +21,7 @@ def get_fid(n, base_size=None, source='choose'):
         file_path = ('/vol/bitbucket/fms119/score_sde_pytorch/samples/'
                      'all_samples_50000.npz')
         data = np.load(file_path)
-        gen_samples = data['images'][:n]
+        gen_samples = data['images'][(i*n):((i+1)*n)]
     elif source=='training':
         file_path = ('/vol/bitbucket/fms119/score_sde_pytorch/samples/'
                      'cifar10_true_trials/cirfar10_true_50000.npz')
@@ -32,14 +34,13 @@ def get_fid(n, base_size=None, source='choose'):
     fid = compute_fid_nchw(data_samples, gen_samples, base_size=base_size)
     return fid
 
-fid_trails = np.zeros((2, len(sizes)))
 
-base_size = '2k'
-
-for j ,source in enumerate(['training', 'generated']):
-    for i, n in enumerate(sizes):
-        fid = get_fid(n, base_size=base_size, source=source)    
-        fid_trails[j, i] = fid
-        # You should always be saving progess during experiments like this.
-        np.savez('/vol/bitbucket/fms119/score_sde_pytorch/samples/cifar10_true_trials/fid_size_experiment_extra_2k.npz', 
-                 fid_trails=fid_trails)
+for i in range(n_trials):
+    fid = get_fid(i, n, base_size=base_size, source=source)    
+    fid_trails[i] = fid
+    print(fid)
+    # You should always be saving progess during experiments like this.
+    save_path = ('/vol/bitbucket/fms119/score_sde_pytorch/samples/'
+                 'cifar10_true_trials/fid_size_experiment_4000_var.npz')
+    np.savez(save_path, 
+                fid_trails=fid_trails)
