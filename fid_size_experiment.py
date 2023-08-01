68a6026cedcdb9f4fabd16114960abe7643ec540
Freddie Sligo-Young <fms119@ic.ac.uk>
2023-08-01 09:51:48 +0100

    Evaluate 4000 generated samples with stretch

--- fid_size_experiment.py
+++ fid_size_experiment.py
@@ -5,47 +5,40 @@ import gc
 from zijing_main import compute_fid_nchw
 
 base_size = '50k'
-source = 'generated'
+source = 'test'
 image_path = '/vol/bitbucket/fms119/score_sde_pytorch/samples/all_samples_4000.npz'
 save_path = ('/vol/bitbucket/fms119/score_sde_pytorch/samples/'
-             f'fid_results/mu_var_1000.npz')
-n = 1000
-n_trials = 10
-fid_trails = np.zeros(n_trials)
-
+            f'fid_results/channel_cov.npz')
 
 def get_fid(i=0, n=0, base_size='50k', source='choose', file_path=''):
     '''
     base_size: str: '2k', '10k' or '50k' or None
     '''
-    # Data not relevant as I have saved statistics
+    #Data not relevant as I have saved statistics
     data_samples = 0.5*np.ones((100, 3, 32, 32))
 
-    if source == 'generated':
+    if source=='generated':
         file_path = ('/vol/bitbucket/fms119/score_sde_pytorch/samples/'
                      'all_samples_50000.npz')
         data = np.load(file_path)
         gen_samples = data['images'][(i*n):((i+1)*n)]
-    elif source == 'training':
+    elif source=='training':
         file_path = ('/vol/bitbucket/fms119/score_sde_pytorch/samples/'
                      'cifar10_true_trials/cirfar10_true_50000.npz')
         data = np.load(file_path)
-        gen_samples = data['images'][:n].transpose(0, 3, 1, 2)
-    elif source == 'test':
+        gen_samples = data['images'][:n].transpose(0,3,1,2)
+    elif source=='test':
         data = np.load(file_path)
         gen_samples = data['images']
 
     gc.collect()
     gen_samples = np.interp(gen_samples, (gen_samples.min(), gen_samples.max()),
-                            (0, 1))
+                            (0, 1))    
     fid = compute_fid_nchw(data_samples, gen_samples, base_size=base_size)
     return fid
 
-
-if __name__ == '__main__':
-    for i in range(n_trials):
-        fid = get_fid(i=i, n=n, base_size=base_size, source=source)
-        fid_trails[i] = fid
-        print(fid)
-        # You should always be saving progess during experiments like this.
-        np.savez(save_path, fid_trails=fid_trails)
+if __name__=='__main__':
+    fid = get_fid(source=source, file_path=image_path)    
+    print(fid)
+    # You should always be saving progess during experiments like this.
+    np.savez(save_path, fid=fid)
