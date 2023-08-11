77b35fd55421155cc75c328c830659ff8618ff9b
Freddie Sligo-Young <fms119@ic.ac.uk>
2023-08-11 20:54:36 +0100

    snr added to --params, can change N

--- generate_samples.py
+++ generate_samples.py
@@ -1,3 +1,16 @@
+from sampling import (ReverseDiffusionPredictor,
+                      RmsCorrector,
+                      LangevinCorrector,
+                      EulerMaruyamaPredictor,
+                      AncestralSamplingPredictor,
+                      NoneCorrector,
+                      NonePredictor,
+                      AnnealedLangevinDynamics)
+import datasets
+from sde_lib import VESDE
+import sampling
+from models import ncsnpp
+from models import utils as mutils
 import argparse
 import gc
 
@@ -11,60 +24,52 @@ from utils import restore_checkpoint
 
 print('Importing')
 
-from models import utils as mutils
-from models import ncsnpp
-
-import sampling
-from sde_lib import VESDE
-from sampling import (ReverseDiffusionPredictor, 
-      				  LangevinCorrector, 
-					  EulerMaruyamaPredictor, 
-					  AncestralSamplingPredictor, 
-					  NoneCorrector, 
-					  NonePredictor,
-					  AnnealedLangevinDynamics)
-import datasets
 
 parser = argparse.ArgumentParser(
-	description='Configure batch sizes and gpu name.'
-	)
-parser.add_argument('-b', '--batch_size', type=int, default=2, 
-					help='Number of images to be generated per machine')
-parser.add_argument('-g', '--gpu', type=str, default='texel05', 
-					help='which GPU in list has this come from')
-parser.add_argument('-c', '--cov', type=str, default=0.6, 
-					help='channel covariance')
-parser.add_argument('-p', '--params', nargs='+', default=[], 
-					help='optuna params list')
+    description='Configure batch sizes and gpu name.'
+)
+parser.add_argument('-b', '--batch_size', type=int, default=64,
+                    help='Number of images to be generated per machine')
+parser.add_argument('-g', '--gpu', type=str, default='ray05',
+                    help='which GPU in list has this come from')
+parser.add_argument('-c', '--cov', type=str, default=0,
+                    help='channel covariance')
+parser.add_argument('-p', '--params', nargs='+', default=[0,0,1,1,0.16],
+                    help='optuna params list')
 args = parser.parse_args()
 
 
 # @title Load the score-based model
-sde = 'VESDE' #@param ['VESDE', 'VPSDE', 'subVPSDE'] {"type": "string"}
+sde = 'VESDE'  # @param ['VESDE', 'VPSDE', 'subVPSDE'] {"type": "string"}
 if sde.lower() == 'vesde':
-	from configs.ve import cifar10_ncsnpp_continuous as configs
-	ckpt_filename = "/vol/bitbucket/fms119/score_sde_pytorch/exp/ve/cifar10_ncsnpp_continuous/checkpoint_24.pth"
-	# THE BELOW CHECKPOINTS DO NOT WORK
-	# ckpt_filename = "/vol/bitbucket/fms119/score_sde_pytorch/exp/ve/cifar10_ncsnpp/checkpoint_16.pth"
-	# ckpt_filename = "/vol/bitbucket/fms119/score_sde_pytorch/exp/ve/cifar10_ncsnpp_deep_continuous/checkpoint_12.pth"
-	# 
-	# I think these do not work because you need to change
-	# from configs.ve import cifar10_ncsnpp_continuous as configs      to
-	# from configs.ve import cifar10_ncsnpp as configs         or
-	# from configs.ve import cifar10_ncsnpp_deep_continuous as configs         
-	# 
-	config = configs.get_config()  
-	sde = VESDE(sigma_min=config.model.sigma_min, sigma_max=config.model.sigma_max, N=config.model.num_scales)
-	sampling_eps = 1e-5
+    from configs.ve import cifar10_ncsnpp_continuous as configs
+    ckpt_filename = "/vol/bitbucket/fms119/score_sde_pytorch/exp/ve/cifar10_ncsnpp_continuous/checkpoint_24.pth"
+    # THE BELOW CHECKPOINTS DO NOT WORK
+    # ckpt_filename = "/vol/bitbucket/fms119/score_sde_pytorch/exp/ve/cifar10_ncsnpp/checkpoint_16.pth"
+    # ckpt_filename = "/vol/bitbucket/fms119/score_sde_pytorch/exp/ve/cifar10_ncsnpp_deep_continuous/checkpoint_12.pth"
+    #
+    # I think these do not work because you need to change
+    # from configs.ve import cifar10_ncsnpp_continuous as configs      to
+    # from configs.ve import cifar10_ncsnpp as configs         or
+    # from configs.ve import cifar10_ncsnpp_deep_continuous as configs
+    #
+    config = configs.get_config()
 
-batch_size =   args.batch_size # 128#@param {"type":"integer"}
+    config.model.num_scales = 100
+
+    sde = VESDE(sigma_min=config.model.sigma_min, sigma_max=config.model.sigma_max,
+                #  This is the number of time steps, originally 1000
+                N=config.model.num_scales)
+    sampling_eps = 1e-5
+
+batch_size = args.batch_size  # 128#@param {"type":"integer"}
 
 config.training.batch_size = batch_size
 config.eval.batch_size = batch_size
 
-random_seed = 0 #@param {"type": "integer"}
-# Stops creation of identical imaged from different machines 
-random_seed = np.random.randint(0, 99999) 
+random_seed = 0  # @param {"type": "integer"}
+# Stops creation of identical imaged from different machines
+random_seed = np.random.randint(0, 99999)
 
 sigmas = mutils.get_sigmas(config)
 scaler = datasets.get_data_scaler(config)
@@ -73,9 +78,9 @@ score_model = mutils.create_model(config)
 
 optimizer = get_optimizer(config, score_model.parameters())
 ema = ExponentialMovingAverage(score_model.parameters(),
-							   decay=config.model.ema_rate)
+                               decay=config.model.ema_rate)
 state = dict(step=0, optimizer=optimizer,
-			 model=score_model, ema=ema)
+             model=score_model, ema=ema)
 
 state = restore_checkpoint(ckpt_filename, state, config.device)
 ema.copy_to(score_model.parameters())
@@ -85,33 +90,37 @@ img_size = config.data.image_size
 channels = config.data.num_channels
 shape = (batch_size, channels, img_size, img_size)
 # Explain reverse diffusion predictor in report.
-predictor = ReverseDiffusionPredictor #@param ["EulerMaruyamaPredictor", "AncestralSamplingPredictor", "ReverseDiffusionPredictor", "None"] {"type": "raw"}
-corrector = LangevinCorrector #@param ["LangevinCorrector", "AnnealedLangevinDynamics", "None"] {"type": "raw"}
+# @param ["EulerMaruyamaPredictor", "AncestralSamplingPredictor", "ReverseDiffusionPredictor", "None"] {"type": "raw"}
+predictor = ReverseDiffusionPredictor
+# @param ["LangevinCorrector", "AnnealedLangevinDynamics", "None"] {"type": "raw"}
+corrector = LangevinCorrector
 
-corrector.cov = args.cov
+# corrector.cov = args.cov
 
-# snr = 0.16 #@param {"type": "number"}
-snr = 0.16 #@param {"type": "number"}
-n_steps =  1#@param {"type": "integer"}
-probability_flow = False #@param {"type": "boolean"}
+params = [float(x) for x in args.params]
+snr = params[-1]  # @param {"type": "number"}
+params = params[:-1]
+
+n_steps = 1  # @param {"type": "integer"}
+probability_flow = False  # @param {"type": "boolean"}
 
 gc.collect()
 
 sampling_fn = sampling.get_pc_sampler(sde, shape, predictor, corrector,
-  									  inverse_scaler, snr, n_steps=n_steps,
-  									  probability_flow=probability_flow,
-  									  continuous=config.training.continuous,
-  									  eps=sampling_eps, device=config.device,
-)
+                                      inverse_scaler, snr, n_steps=n_steps,
+                                      probability_flow=probability_flow,
+                                      continuous=config.training.continuous,
+                                      eps=sampling_eps, device=config.device,
+                                      )
 
-params = [float(x) for x in args.params]
 
 x, n = sampling_fn(score_model, args.gpu, *params)
+# x, n = sampling_fn(score_model, args.gpu)
 
 numpy_x = x.cpu().numpy()
-		
+
 # while not validate_images({'x':numpy_x}):
-	
+
 # 	print('caught failed before saving files')
 # 	print(f'std: {numpy_x.reshape(-1).std()}')
 
@@ -119,4 +128,4 @@ numpy_x = x.cpu().numpy()
 # 	numpy_x = x.cpu().numpy()
 
 np.savez(f'/vol/bitbucket/fms119/score_sde_pytorch/samples/'
-		 f'{args.gpu}_samples.npz', x=numpy_x)
+         f'{args.gpu}_samples.npz', x=numpy_x)
